5b67672a03f53b993b58faac810fd16f82cc7826
giannirelae <giannirelae@gmail.cm>
2021-08-10 23:21:52 +0200

    test

--- web.js
+++ web.js
@@ -10,17 +10,17 @@ app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
 });
 
-var server = app.listen(process.env.PORT, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log(host)
-  console.log('Web server started at http://%s:%s', host, port);
-});
+// var server = app.listen(process.env.PORT, function () {
+//   var host = server.address().address;
+//   var port = server.address().port;
+//   console.log(host)
+//   console.log('Web server started at http://%s:%s', host, port);
+// });
 
 module.exports = function (bot) {
-  console.log(process.env.BOT_TOKEN)
-  app.post(`/${process.env.BOT_TOKEN}`, (req, res) => {
-    bot.processUpdate(req.body);
-    res.status(200).json({ message: 'ok' });
-});
+//   console.log(process.env.BOT_TOKEN)
+//   app.post(`/${process.env.BOT_TOKEN}`, (req, res) => {
+//     bot.processUpdate(req.body);
+//     res.status(200).json({ message: 'ok' });
+// });
 };
