779b76a01fe2eb993e7e3937300394f84a379a4c
giannirelae <giannirelae@gmail.cm>
2021-08-10 22:56:35 +0200

    ddddd

--- web.js
+++ web.js
@@ -13,11 +13,13 @@ app.get('/', function (req, res) {
 var server = app.listen(process.env.PORT, function () {
   var host = server.address().address;
   var port = server.address().port;
-
+  console.log(process.env.BOT_TOKEN)
+  console.log(host)
   console.log('Web server started at http://%s:%s', host, port);
 });
 
 module.exports = function (bot) {
+  console.log(process.env.BOT_TOKEN)
   app.post(`/${process.env.BOT_TOKEN}`, (req, res) => {
     bot.processUpdate(req.body);
     res.status(200).json({ message: 'ok' });
